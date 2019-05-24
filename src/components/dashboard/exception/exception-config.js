fca896db538fdd9024d52b5beca7f26ee1942f58
sebamed <seba.med@yahoo.com>
2019-05-24 18:31:38 +0200

    Added 403 and 500 images

--- src/components/dashboard/exception/exception-config.js
+++ src/components/dashboard/exception/exception-config.js
@@ -10,5 +10,18 @@ export const ExceptionConfig = {
         status: '404',
         description: <FormattedMessage id='exception.404.description' />,
         button: <Button type='primary' onClick={() => history.push(ROUTE_DASHBOARD)}><FormattedMessage id='exception.404.button' /></Button>
+    },
+    '403': {
+        img: 'https://gw.alipayobjects.com/zos/rmsportal/wZcnGqRDyhPOEYFcZDnb.svg',
+        status: '403',
+        description: <FormattedMessage id='exception.403.description' />,
+        button: <Button type='primary' onClick={() => history.push(ROUTE_DASHBOARD)}><FormattedMessage id='exception.403.button' /></Button>
+
+    },
+    '500': {
+        img: 'https://gw.alipayobjects.com/zos/rmsportal/RVRUAYdCGeYNBWoKiIwB.svg',
+        status: '500',
+        description: <FormattedMessage id='exception.500.description' />,
+        button: <Button type='primary' onClick={() => history.push(ROUTE_DASHBOARD)}><FormattedMessage id='exception.500.button' /></Button>
     }
 }
